0391404828774dec450b6ba43ee480a92d5d6e5d
John <johnolshea@gmail.com>
2024-07-30 22:59:05 +0000

    Fix eslint errors

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,11 @@
-function getListStudents() {
-	return [
-		{id:1, firtName: 'Guillaume', location: 'San Francisco'},
-		{id:2, firtName: 'James', location: 'Columbia'},
-		{id:3, firtName: 'Serena', location: 'San Francisco'},
-	]
-}
+const getListStudents = () => {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+};
 
 export default getListStudents;
